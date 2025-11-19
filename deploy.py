854bc40d708ab9ed0a07814f55c1250c103db0ea
Peter VandeHaar <pjvandehaar@gmail.com>
2025-11-19 13:58:37 -0500

    .

--- deploy.py
+++ deploy.py
@@ -7,10 +7,15 @@ def run_cmd(cmd: list[str]) -> None:
     print('=>', cmd)
     subp.run(cmd)
 
+def strict_replace(text: str, old: str, new: str) -> str:
+    if old not in text:
+        raise ValueError(f"String '{old}' not found in text")
+    return text.replace(old, new)
+
 html = Path('index.html').read_text()
 script = Path('script.js').read_text()
 
-new_html = html.replace('<script src="script.js"></script>', f'<script type="text/javascript">{script}</script>')
+new_html = strict_replace(html, '<script src="script.js"></script>', f'<script type="text/javascript">{script}</script>')
 Path('1.html').write_text(new_html)
 
 run_cmd(['scp', '1.html', 'petervh:/var/www/html/tmp/katakana-trainer/index.html'])
